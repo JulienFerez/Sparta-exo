784a0362c13c6fb8ac49cb9e1ead16eb415a116b
Julien Ferez <Julienferez@icloud.com>
2022-01-12 14:36:52 +0100

    exo6finish

---
+++ 02_programming/02_conditions/a136f453a4ff29934baae3372d41abb3f52d90ba/06_conditional_statement_drink/src/index.js
@@ -0,0 +1,34 @@
+// > Frieda and Francis walk on the street. Frieda is 22 and Francis is 17.
+
+// Create two objects, `frieda`, `francis` with the fields :
+//   `age` (an integer) and `name`, (obviously a string)
+const frieda = {
+  name : "Frieda",  
+  age : 22  
+} ;
+
+const francis = {
+  name : "Francis", 
+  age : 17 
+} ;
+
+
+// Create two variables `canFriedaDrinkAlcohol` and `canFrancisDrinkAlcohol` to answer,
+// respectively, to the question "Can he / she drink alcohol?".
+
+let canFriedaDrinkAlcohol;
+
+if (frieda.age > 18) {
+  canFriedaDrinkAlcohol = true;
+} else {
+  canFriedaDrinkAlcohol = false;
+}
+
+
+let canFrancisDrinkAlcohol;
+
+if (francis.age > 18) {
+  canFrancisDrinkAlcohol = true;
+} else {
+  canFrancisDrinkAlcohol = false;
+}

1427c8711f584cf63fab601dbcc4d590151c91dc
Julien Ferez <Julienferez@icloud.com>
2022-01-12 22:12:10 +0100

    exo8finish

--- 02_programming/02_conditions/a136f453a4ff29934baae3372d41abb3f52d90ba/08_object_values/src/index.js
+++ 02_programming/02_conditions/a136f453a4ff29934baae3372d41abb3f52d90ba/08_object_values/src/index.js
@@ -16,10 +16,11 @@ const armouryWithNamesOnly = Object.values(armoury);
 
 // Create a constant isTheRequestInArmoury bound to the check of the presence
 // of odinRequest in armouryWithNamesOnly
-
+const isTheRequestInArmoury = Object.values(armoury).includes(odinRequest);
 
 
 
 // Create a condition that logs "Ah, I finally found something to hammer that nail into"
 // if the isTheRequestInArmoury is true and "What do you want me to do with this?" if not
 
+isTheRequestInArmoury ? console.log("Ah, I finally found something to hammer that nail into") : console.log("What do you want me to do with this?") ; 

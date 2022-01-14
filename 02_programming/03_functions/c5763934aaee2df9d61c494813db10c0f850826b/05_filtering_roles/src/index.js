1659313861e659b9c17d4ca495307b8bd7964fe9
Julien Ferez <Julienferez@icloud.com>
2022-01-14 10:26:16 +0100

    05 no finish

---
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/05_filtering_roles/src/index.js
@@ -0,0 +1,30 @@
+function filteredRoles(persons, role = "teacher"){
+  if (arrayPersons[0].role === role){return arrayPersons.push[0];}
+  if (arrayPersons[1].role === role){return arrayPersons.push[1];}
+  if (arrayPersons[2].role === role){return arrayPersons.push[2];}
+  if (arrayPersons[3].role === role){return arrayPersons.push[3];}
+}
+
+const arrayPersons = [
+  {
+    persons : "Julien",
+    role : "Teacher"
+  },
+  {
+    persons : "Estelle",
+    role : "Teacher"
+  },
+  {
+    persons : "Richard",
+    role : "Student",
+  },
+  {
+    persons : "",
+    role : "Student",
+  },
+];
+
+
+// Do not remove lines below, it is for tests
+module.exports = filteredRoles;
+

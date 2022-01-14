c7f7e0457de9da895c49140b1e5fd9aad2743159
Julien Ferez <Julienferez@icloud.com>
2022-01-14 11:33:40 +0100

    01finish

---
+++ 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/01_create_callback/src/callback.js
@@ -0,0 +1,9 @@
+// code here
+
+const sayHello = (player) => {console.log(`Hello, ${player.name}`);};
+console.log(sayHello);
+
+
+
+// leave the code below for tests to work
+module.exports = sayHello;

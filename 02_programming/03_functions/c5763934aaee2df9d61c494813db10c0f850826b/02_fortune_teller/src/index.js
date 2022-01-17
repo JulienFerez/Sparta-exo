e9aa971560477793f8eede584f45ca1ff3ddba7f
Julien Ferez <Julienferez@icloud.com>
2022-01-17 10:12:51 +0100

    exo 2

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/02_fortune_teller/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/02_fortune_teller/src/index.js
@@ -1,7 +1,9 @@
-function tellFortune(job,city,name,numberofyears) {
-  console.log(`You will be ${job} in ${city}, and married to ${name} in ${numberofyears} years.`);
+function tellFortune(job,city,name,numberYears){
+  return console.log(`You will be ${job} in ${city}, and married to ${name} in ${numberYears} years.`);
 }
-tellFortune("webdevelopper","lille","estelle",2);
+tellFortune("dev","lille","estelle",2024);
 
 // Do not remove last lines, it is for tests
 module.exports = tellFortune;
+
+

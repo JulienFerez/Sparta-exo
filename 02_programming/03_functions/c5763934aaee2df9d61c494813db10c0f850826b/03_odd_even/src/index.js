e7ff1b7e803c2a52974772edf50a4c61a745bff9
Julien Ferez <Julienferez@icloud.com>
2022-01-17 10:13:13 +0100

    exo 3

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/03_odd_even/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/03_odd_even/src/index.js
@@ -1,12 +1,14 @@
-function oddEven(number) {
-  if (Number.isInteger(number)) {
-    return number% 2 === 0 ?  `${number} is an even number.` : `${number} is an odd number.`;
-  } else {
-    return console.log(`${number} is not a number.`);
-  }
+function oddEven(number){
+  if (Number.isInteger(number))
+  { return number%2 === 0 ? `${number} is an even number.` : `${number} is an odd number.`; }else {console.log(`${number} is not a number.`);}
 }
+
+oddEven(3);
+oddEven(6);
+oddEven("afergdgf");
 
 // Do not remove last lines, it is for tests
 module.exports = oddEven;
+
 
 

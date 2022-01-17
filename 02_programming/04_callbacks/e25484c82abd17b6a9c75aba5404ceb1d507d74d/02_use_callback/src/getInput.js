bf1695a41d946574805598717ad6a2c592ac0038
Julien Ferez <Julienferez@icloud.com>
2022-01-17 10:01:38 +0100

    02 exo

---
+++ 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/02_use_callback/src/getInput.js
@@ -0,0 +1,10 @@
+// A function getInput that takes two parameters, an input (the first one) and a callback function
+
+// code the function here
+
+function getInput (getInput,callback){
+  return console.log(getInput.key);
+};
+
+// Leave the line below for tests to work
+(module.exports = getInput);

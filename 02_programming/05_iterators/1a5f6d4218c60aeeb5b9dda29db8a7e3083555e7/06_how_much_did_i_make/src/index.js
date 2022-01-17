804c4e20776fb60866ccebf66d83f01aa1dc03d6
Julien Ferez <Julienferez@icloud.com>
2022-01-17 18:59:14 +0100

    exo6finish

---
+++ 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/06_how_much_did_i_make/src/index.js
@@ -0,0 +1,17 @@
+function howMuchDidIMake(invoices) {
+
+  return invoices
+    .filter((invoices) => invoices.waiter === "Yourself" )
+    .map((arraytip) => arraytip.tip )
+    .reduce((a,b) => a + b,0);
+
+}
+
+const invoices = [
+  { clients: 3, waiter: "Yourself", price: 5100, tip: 200 },
+  { clients: 2, waiter: "Dominique", price: 2200, tip: 50 },
+  { clients: 2, waiter: "Yourself", price: 2900, tip: 100 },
+];
+
+// Do not remove the following line, it is for tests
+module.exports = howMuchDidIMake;

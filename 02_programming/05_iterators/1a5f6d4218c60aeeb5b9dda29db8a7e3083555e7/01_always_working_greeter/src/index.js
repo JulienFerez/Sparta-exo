a630dd1f8d51ce61dad9768669932aa9dbd83244
Julien Ferez <Julienferez@icloud.com>
2022-01-17 11:20:04 +0100

    exo 1 finish

---
+++ 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/01_always_working_greeter/src/index.js
@@ -0,0 +1,11 @@
+function greeter(listOfPeople) {
+  listOfPeople.forEach((person)=>{
+    console.log(`Hello ${person}!`);
+  });
+}
+
+
+
+
+// Do not remove the following lines, it is for tests
+module.exports = greeter;

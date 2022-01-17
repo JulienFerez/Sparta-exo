872e11ddb3bee6c3dd1d65e734e425976e439dc7
Julien Ferez <Julienferez@icloud.com>
2022-01-17 18:39:17 +0100

    exo5finish

---
+++ 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/05_only_my_tips/src/index.js
@@ -0,0 +1,14 @@
+function onlyMyTips(invoices) {
+  return invoices
+    .filter((invoices) => invoices.waiter === "Yourself" )
+    .map((arraytip) => arraytip.tip );
+}
+
+const todaysInvoices = [
+  { clients: 3, waiter: "Yourself", price: 5100, tip: 200 },
+  { clients: 2, waiter: "Dominique", price: 2200, tip: 50 },
+  { clients: 2, waiter: "Yourself", price: 2900, tip: 100 },
+];
+
+// Do not remove the following line, it is for tests
+module.exports = onlyMyTips;

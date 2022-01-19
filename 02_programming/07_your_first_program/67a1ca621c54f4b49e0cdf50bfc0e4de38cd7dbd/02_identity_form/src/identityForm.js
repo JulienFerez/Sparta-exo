d81b11d256624748e528ee626819ce59a884ac49
Julien Ferez <Julienferez@icloud.com>
2022-01-19 11:33:03 +0100

    03_number_gamefinish

---
+++ 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/02_identity_form/src/identityForm.js
@@ -0,0 +1,13 @@
+const identityForm = (reader) => {
+  reader.question("What is your first name? " , (reponseFirstName) => { reader.question("What is your last name? " , (reponseLastName)=> {reader.question("age" , (reponseAge)=> {
+    console.log(`Your name is ${reponseFirstName} ${reponseLastName} and you are ${reponseAge}.`);
+    reader.close();
+  });});});
+
+
+};
+
+// Leave line below for tests to work
+module.exports = identityForm;
+
+

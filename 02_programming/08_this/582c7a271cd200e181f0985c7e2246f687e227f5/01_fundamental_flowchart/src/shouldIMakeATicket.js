ceda16a16af8d36f7f79d0d9f64d492f6397714d
Julien Ferez <Julienferez@icloud.com>
2022-01-20 11:07:36 +0100

    exo1finish

---
+++ 02_programming/08_this/582c7a271cd200e181f0985c7e2246f687e227f5/01_fundamental_flowchart/src/shouldIMakeATicket.js
@@ -0,0 +1,12 @@
+function shouldIMakeATicket(isStuck, timeBeingStuck, hourOfTheDay){
+
+  if (isStuck === true && timeBeingStuck >= 20 && ((hourOfTheDay >= 9 && hourOfTheDay < 13)||(hourOfTheDay > 13 && hourOfTheDay < 18))){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+// Don't touch this for tests to execute properly
+module.exports = shouldIMakeATicket;

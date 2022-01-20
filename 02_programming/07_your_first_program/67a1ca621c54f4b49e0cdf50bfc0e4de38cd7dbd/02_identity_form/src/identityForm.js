4532f18e93bf89f44db2c4d89dbfc508ffe48631
Julien Ferez <Julienferez@icloud.com>
2022-01-20 09:02:30 +0100

    exo 2

--- 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/02_identity_form/src/identityForm.js
+++ 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/02_identity_form/src/identityForm.js
@@ -1,5 +1,8 @@
 const identityForm = (reader) => {
-  reader.question("What is your first name? " , (reponseFirstName) => { reader.question("What is your last name? " , (reponseLastName)=> {reader.question("age" , (reponseAge)=> {
+  reader.question("What is your first name? " , (reponseFirstName) =>
+  { reader.question("What is your last name? " , (reponseLastName)=>
+  {reader.question("age" , (reponseAge)=>
+  {
     console.log(`Your name is ${reponseFirstName} ${reponseLastName} and you are ${reponseAge}.`);
     reader.close();
   });});});

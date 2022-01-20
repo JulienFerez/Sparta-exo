e3f350ba486ffe88e14f304f57e05e72d3a3f9f0
Julien Ferez <Julienferez@icloud.com>
2022-01-20 20:03:40 +0100

    exo 5 nofinish

---
+++ 02_programming/08_this/582c7a271cd200e181f0985c7e2246f687e227f5/05_human_factory/src/humanFactory.js
@@ -0,0 +1,13 @@
+const humanFactory = (humanData = {}) => {
+  // code here
+};
+
+const createHumans = (humans) => {
+  const humans = createHumans([{ lastName: "Toto" }, { genre: "female" }]);
+
+};
+
+module.exports = {
+  humanFactory,
+  createHumans,
+};

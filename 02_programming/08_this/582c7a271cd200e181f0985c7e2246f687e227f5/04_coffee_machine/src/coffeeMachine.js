ab2cb0bf3f18fe5e4d0db36f45893c8f6f823b51
Julien Ferez <Julienferez@icloud.com>
2022-01-20 18:36:28 +0100

    exo4finish

---
+++ 02_programming/08_this/582c7a271cd200e181f0985c7e2246f687e227f5/04_coffee_machine/src/coffeeMachine.js
@@ -0,0 +1,23 @@
+const machine = {
+
+  litersOfCoffee : 0 ,
+
+  fillWithLitersOfCoffee : function(litreAdd) {
+    this.litersOfCoffee += litreAdd ;
+    return this;
+  },
+
+  expresso : function () {
+    if(this.litersOfCoffee >= 0.08) { this.litersOfCoffee -= 0.08; return  true ;} else { return false ;}
+  },
+
+  longCoffee : function () {
+    if(this.litersOfCoffee >= 0.15) { this.litersOfCoffee -= 0.15 ; return  true ;} else { return false ;}
+
+  }};
+
+
+
+
+
+module.exports = machine;

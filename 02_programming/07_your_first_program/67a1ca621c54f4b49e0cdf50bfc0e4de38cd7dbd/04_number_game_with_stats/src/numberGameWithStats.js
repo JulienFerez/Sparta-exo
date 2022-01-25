90ce2d6e9bc2b17ab81c9d2e944fc8876f05163a
Julien Ferez <Julienferez@icloud.com>
2022-01-25 16:07:31 +0100

    exo 04 numbergamewithstats

--- 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/04_number_game_with_stats/src/numberGameWithStats.js
+++ 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/04_number_game_with_stats/src/numberGameWithStats.js
@@ -1,5 +1,61 @@
+// Message de bienvenue
+
+console.log("Welcome!");
+console.log("You have to find the right number, between 1 and 100!");
+console.log("Good luck!!");
+
+// Formule du nombre aléatoire
+
+function numberRandom(min, max) {
+  return Math.round(Math.random() * (max - min)) + min;
+}
+
+// Function principale
+
 function numberGameWithStats(reader, min = 1, max = 100) {
-  // Code here
+  const numberServer = numberRandom(min, max);
+  console.log(numberServer);
+
+  function question(counter = 0) {
+    let functionCounter = counter;
+    console.log(functionCounter);
+    reader.question("Enter a number ", (numberClient) => {
+      let userNumber = parseInt(numberClient);
+      functionCounter++;
+
+      if (!Number.isInteger(userNumber)) {
+        console.log("This was not a number");
+
+        question(functionCounter);
+      } else if (userNumber > max || userNumber < min) {
+        console.log(`The number is between ${min} and ${max}`);
+
+        question(functionCounter);
+      } else if (userNumber < numberServer) {
+        console.log("Too low");
+
+        question(functionCounter);
+      } else if (userNumber > numberServer) {
+        console.log("Too high");
+
+        question(functionCounter);
+      } else {
+        console.log("You won!");
+
+
+        if (functionCounter > 1) {
+          console.log(`Vous avez reussi en ${functionCounter} fois`);
+        } else {
+          console.log("Félictation vous avec reussi du premier coup");
+        }
+
+        reader.close();
+      }
+
+      //console.log(numberClient);
+    });
+  }
+  question();
 }
 
 module.exports = numberGameWithStats;

1df662624d3913fbcbf236e841e03c43983fe685
Julien Ferez <Julienferez@icloud.com>
2022-01-26 16:30:43 +0100

    recursion fini

--- 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/06_recursion/src/recursion.js
+++ 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/06_recursion/src/recursion.js
@@ -1,6 +1,30 @@
-const findPosition = () => {
-  // Code here
+
+
+
+
+
+const findPosition = (listOfPerson, person, i) => {
+//  console.log(`${i }Ligne7`);
+  if(listOfPerson.every(checkIfString) && typeof person === "string" && Number.isInteger(i)){
+    if (person === listOfPerson[i]){
+      return `${person} is at position #${i+1} in this array.`;
+    } else {
+      if (i < listOfPerson.length ){
+        return findPosition(listOfPerson, person, i+1);
+      } else {
+        return `${person} is not present in this array.`;
+      };
+
+    }
+  } else {
+    throw new Error("Error");
+  }
 };
+
+function checkIfString (elementArray){
+  return typeof elementArray === "string";
+}
+
 
 // Leave line below for tests to work properly
 module.exports = findPosition;

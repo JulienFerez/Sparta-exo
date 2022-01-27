f91943e9ed063f01058b056a7ff518a575a306ee
Julien Ferez <Julienferez@icloud.com>
2022-01-27 14:14:41 +0100

    Exo draw me a square

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/05_draw_me_a_square/src/index.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/05_draw_me_a_square/src/index.js
@@ -1,5 +1,16 @@
-function square() {
-  // Code here
+function square(squareSize) {
+
+  // Code
+
+  let line = "";
+
+  for (let i = 0 ; i < squareSize; i++) {
+    line += "*" ;
+  }
+
+  for (let i = 0 ; i < squareSize; i++) {
+    console.log(line);
+  }
 }
 
 // Do not remove the following line, it is for tests

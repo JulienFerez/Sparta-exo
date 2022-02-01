26d942fc08e8126501b3f150d96c8d2a82280e03
Julien Ferez <Julienferez@icloud.com>
2022-02-01 08:59:21 +0100

    clean branc Exo 7 Bonus à faire

---
+++ 02_programming/15_typescript_101/f284fbcc79bce170e7c536c6c6a19b31653e10bb/07_holiday_location_finder_bonus/src/holidayLocationFinder.ts
@@ -0,0 +1,21 @@
+import { Interface } from "readline";
+
+console.log(` Welcome to HOLIDAY LOCATION FINDER
+
+  This program will help you find a location based on the continent you will input.
+
+  Here is the list of continent to chose from:
+  - Europe
+  - North America
+  - South America
+  - Asia
+  - Africa
+  - Oceania
+`);
+
+function holidayLocationFinder(reader: Interface) {
+  // Write your code here
+}
+
+// Leave the line below for tests to work properly.
+export { holidayLocationFinder };

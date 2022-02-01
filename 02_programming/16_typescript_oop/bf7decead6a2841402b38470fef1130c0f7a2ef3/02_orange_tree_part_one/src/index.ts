75c8696e16b0727197e9a429f7f75e1ac1b6b4ec
Julien Ferez <Julienferez@icloud.com>
2022-02-01 11:10:30 +0100

    Exo 2 Orange Tree part one

---
+++ 02_programming/16_typescript_oop/bf7decead6a2841402b38470fef1130c0f7a2ef3/02_orange_tree_part_one/src/index.ts
@@ -0,0 +1,32 @@
+// Code the class here.
+class Tree {
+  // Properties member.
+  age: number;
+  height: number;
+  alive: boolean = true;
+
+  // Constructor member.
+  constructor(age: number) {
+    this.age = age;
+    this.height = this.Calculeheight();
+  }
+
+  // Methods member.
+  Calculeheight(): number {
+    if (this.age <= 9) {
+      return this.age * 25;
+    } else if (this.age <= 20) {
+      return 9 * 25 + (this.age - 9) * 10;
+    } else {
+      return 9 * 25 + 11 * 10;
+    }
+  }
+
+  seed(age: number, height: number, living: boolean): any {
+    this.age = 0;
+    this.height = 0;
+    this.alive = true;
+  }
+}
+// Leave the line below for tests to work properly.
+export { Tree };

c77de9f5ba47959ee03b7fa8d33f583e0a383531
Julien Ferez <Julienferez@icloud.com>
2022-02-21 10:16:49 +0100

    exo 1 warmump greeteer function

---
+++ 04_backend_web_development_and_api/06_promises/35e1206269e67dfaabf4d1752df23348439eda2e/01_warmup_greeter_function/src/greetEveryone.ts
@@ -0,0 +1,8 @@
+type ParamRest = string[];
+
+export function greetEveryone(...people: string[]): void {
+  // Code your function here
+  people.forEach((person) => {
+    console.log(`Welcome to ${person}`);
+  });
+}

964d71ba72b3fff3e398ab4f6e3a596cdf59a4f5
Julien Ferez <Julienferez@icloud.com>
2022-03-15 16:01:24 +0100

    exo03 forrms and validations

--- 06_fullstack_web_development/01_react_functional_components/fe2008a224b48c8983a2d34ead9d860fe99af1ed/03_forms_and_validations/src/components/Form.jsx
+++ 06_fullstack_web_development/01_react_functional_components/fe2008a224b48c8983a2d34ead9d860fe99af1ed/03_forms_and_validations/src/components/Form.jsx
@@ -1,5 +1,35 @@
 import React from "react";
 
-const Form = () => "Code here";
+const Form = () => {
+  const [space, setSpace] = React.useState(false);
+
+  function validateInput(value) {
+    value.includes(" ") ? setSpace(!space) : null;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label htmlFor="exampleInputEmail1">First Name</label>
+        <input
+          onChange={(event) => validateInput(event.target.value)}
+          type="text"
+          className="form-control"
+          id="exampleFirstName"
+        />
+        <small id="FirstName Help" className="form-text text-muted"></small>
+        {space ? "No Spaces Allowed" : null}
+      </div>
+
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+};
 
 export default Form;

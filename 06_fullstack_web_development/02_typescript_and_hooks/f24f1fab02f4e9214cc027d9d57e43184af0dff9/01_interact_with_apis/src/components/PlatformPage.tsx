6c18d9b5beacfebcd4df6dc51b014ac8c4dfbbc0
Julien Ferez <Julienferez@icloud.com>
2022-03-16 23:59:47 +0100

    no finish details of games

--- 06_fullstack_web_development/02_typescript_and_hooks/f24f1fab02f4e9214cc027d9d57e43184af0dff9/01_interact_with_apis/src/components/PlatformPage.tsx
+++ 06_fullstack_web_development/02_typescript_and_hooks/f24f1fab02f4e9214cc027d9d57e43184af0dff9/01_interact_with_apis/src/components/PlatformPage.tsx
@@ -1,7 +1,75 @@
 import React from "react";
 
+type Platform = {
+  name: string;
+  id: string;
+};
+
+type Games = {
+  name: string;
+  id: string;
+};
+
 const PlatformPage = (): JSX.Element => {
-  return <>Platforms</>;
+  const [platform, setPlatform] = React.useState([]);
+  const [game, setGames] = React.useState([]);
+  const [idPlatform, setidPlatform] = React.useState("");
+  const [idGame, setidGame] = React.useState("");
+
+  React.useEffect(() => {
+    fetch("http://videogame-api.fly.dev/platforms")
+      .then((response) => response.json())
+      .then((result) => {
+        const arrayOfPlatform = result.platforms.map((platform: Platform) => {
+          return { name: platform.name, id: platform.id };
+        });
+        setPlatform(arrayOfPlatform);
+      });
+  }, []);
+
+  React.useEffect(() => {
+    fetch(`http://videogame-api.fly.dev/games/platforms/${idPlatform}`)
+      .then((response) => response.json())
+      .then((result) => {
+        const arrayOfGames = result.games.map((games: Games) => {
+          return { name: games.name, id: games.id };
+        });
+        setGames(arrayOfGames);
+      });
+  }, [idPlatform]);
+
+  // Display details of games
+  React.useEffect(() => {
+    console.log(idGame);
+    fetch(`
+    http://videogame-api.fly.dev/games/${idGame}`)
+      .then((response) => response.json())
+      .then((result) => {
+        const arrayOfGames = result.games.map((games: Games) => {
+          return { name: games.name, id: games.id };
+        });
+        setGames(arrayOfGames);
+      });
+  }, [idGame]);
+
+  return (
+    <>
+      <select onChange={(event) => setidPlatform(event.target.options[event.target.selectedIndex].id)}>
+        {platform?.map((element: any) => (
+          <option key={element.id} id={element.id}>
+            {element.name}
+          </option>
+        ))}
+      </select>
+      <select onChange={(event) => setidGame(event.target.options[event.target.selectedIndex].id)}>
+        {game?.map((element: any) => (
+          <option key={element.id} id={element.id}>
+            {element.name}
+          </option>
+        ))}
+      </select>
+    </>
+  );
 };
 
 export default PlatformPage;
